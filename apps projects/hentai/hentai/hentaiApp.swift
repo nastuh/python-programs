9899845db49d8aaae528818483320151726e3657
nastuh <139132017+nastuh@users.noreply.github.com>
2025-10-12 16:21:41 +0300

    Add files via upload

---
+++ apps projects/hentai/hentai/hentaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  hentaiApp.swift
+//  hentai
+//
+//  Created by di9star on 22.07.25.
+//
+
+import SwiftUI
+
+@main
+struct hentaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ apps projects/hentai/hentaiTests/hentaiTests.swift
@@ -0,0 +1,16 @@
+//
+//  hentaiTests.swift
+//  hentaiTests
+//
+//  Created by di9star on 22.07.25.
+//
+
+import Testing
+
+struct hentaiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
